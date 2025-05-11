de06af0c02b02ddbb5da4362a5403bafd8fb5abf
artemkata2211 <poxitivik@yandex.ru>
2025-05-11 18:27:38 +0500

    first

---
+++ script.js
@@ -0,0 +1,38 @@
+
+const swiper = new Swiper('.swiper', {
+  loop: false,
+  pagination: {
+    el: '.swiper-pagination',
+    clickable : true ,
+  },
+  hashNavigation: true,
+  slidesPerView : 1.7,
+  mousewheel: true,
+  keyboard: true,
+  freeMode: true,
+  width: 330,
+  height: 80,
+  breakpoints: { 320: { enabled: true, },
+   768: { enabled: false, } }
+}); 
+
+let button = document.querySelector('.content__contiunuation');
+let hiddens = document.querySelectorAll('.hidden');
+
+button.onclick = function () {
+for (let hidden of hiddens) {
+  if (hidden.classList.contains('hidden')) {
+    button.classList.toggle('icon');
+hidden.classList.toggle('hidden');
+button.textContent = 'Скрыть';
+button.style.backgroundImage = './images/icon.svg'
+  } else {
+     hidden.classList.add('hidden');
+    button.textContent = 'Показать еще';
+    button.classList.remove('icon');
+  }
+}
+}
+
+
+
